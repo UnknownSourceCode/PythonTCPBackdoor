41d15a1822b6f71d342033b150537c82990e580f
UnknownSourceCode <53025923+UnknownSourceCode@users.noreply.github.com>
2019-07-18 11:17:21 -0500

    Create PythonTCPBackdoor.py

---
+++ PythonTCPBackdoor.py
@@ -0,0 +1,69 @@
+import socket
+import threading
+import time
+
+print ("Hello To PythonTCPBackdoor")
+time.sleep(5)
+TargetIP = input("Give Me The Target IP:")
+TargetPort = input("Give Me The Target Port:")
+bind_ip = TargetIP
+bind_port = TargetPort
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+server.listen(5)
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+
+# this is our client-handling thread
+def handle_client(client_socket):
+
+    # print out what the client sends
+    request = client_socket.recv(1024)
+    print "[*] Received: %s" % request
+
+    # send back a packet
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+
+    client,addr = server.accept()
+    print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+
+    # spin up our client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()import socket
+import threading
+import time
+
+print ("Hello To PyTCPNetcat")
+time.sleep(5)
+TargetIP = input("Give Me The Target IP:")
+TargetPort = input("Give Me The Target Port:")
+bind_ip = TargetIP
+bind_port = TargetPort
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip,bind_port))
+server.listen(5)
+print "[*] Listening on %s:%d" % (bind_ip,bind_port)
+
+# this is our client-handling thread
+def handle_client(client_socket):
+
+    # print out what the client sends
+    request = client_socket.recv(1024)
+    print "[*] Received: %s" % request
+
+    # send back a packet
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+
+    client,addr = server.accept()
+    print "[*] Accepted connection from: %s:%d" % (addr[0],addr[1])
+
+    # spin up our client thread to handle incoming data
+    client_handler = threading.Thread(target=handle_client,args=(client,))
+    client_handler.start()
